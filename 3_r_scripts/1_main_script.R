d6720e5873cf8e718125c7e2042f5c9176c55e97
Jennifer Uehling <juehling@MacBook-Pro.local>
2021-02-26 13:36:32 -0500

    Filtered for Paige's project, updated sample data file, started makaing figures

--- 3_r_scripts/1_main_script.R
+++ 3_r_scripts/1_main_script.R
@@ -1,6 +1,6 @@
 ###
 # Written by: Conor Taff, Jenny Uehling, and Paige Becker
-# Last updated: 2/22/2021
+# Last updated: 2/24/2021
 # Run under R Studio XX on XX
 
 # This is the main analysis script for processing the COI data after running through
@@ -28,85 +28,195 @@ pacman::p_load("tidyverse", "phyloseq", "plyr", "vegan", "here", "ggpubr", "igra
 # the prefix used for AMPtk processing
 # this is set in amptk and all files produced there have this prefix
 # when adapting this to a different set of data, change prefix
-amptk_prefix <- "trescoi"   
+amptk_prefix <- "trescoi"
+
+# For now, we're going to need to process the sequencing runs from November and 
+# December separately.
+
+############################# November Run (11)
 
 # Load the number of reads by taxa per sample table. Format for phyloseq.
-otu_ab <- read.delim(here("1_raw_data", paste0(amptk_prefix, ".cluster.otu_table.txt")))
-rownames(otu_ab) <- otu_ab$X.OTU.ID   # give rownames from sample names
-otu_ab <- otu_ab[, 2:ncol(otu_ab)]    # remove the column of sample names
+otu_ab_11 <- read.delim(here("1_raw_data", paste0(amptk_prefix, "_2020_11.cluster.otu_table.txt")))
+otu_ab_11$X.OTU.ID <- paste0(otu_ab_11$X.OTU.ID, "_11") # delineate as Nov seq run
+rownames(otu_ab_11) <- otu_ab_11$X.OTU.ID   # give rownames from sample names
+otu_ab_11 <- otu_ab_11[, 2:ncol(otu_ab_11)]    # remove the column of sample names
 
 # Read the mapping table
 # this 'mapping' table from AMPtk is mostly blank but has all the sample
 # names so it can be joined to actual sample metadata. It's also possible
 # to merge in the sample metadata within the AMPtk pipeline.
-map <- read.delim(here("1_raw_data", paste0(amptk_prefix, ".mapping_file.txt")))
+map_11 <- read.delim(here("1_raw_data", paste0(amptk_prefix, "_2020_11.mapping_file.txt")))
 
-for(i in 1:nrow(map)){
-  map$sampleID[i] <- strsplit(map$X.SampleID[i], "x")[[1]][2]
+for(i in 1:nrow(map_11)){
+  map_11$sampleID[i] <- strsplit(map_11$X.SampleID[i], "x")[[1]][2]
 }
 
-# write.table(map, "map.txt", sep = "\t") # if you want to save a copy of the mapping file
+# write.table(map_11, "map_11.txt", sep = "\t") # if you want to save a copy of the mapping file
 
 # Read the otu taxonomy table
-otu_tax <- read.delim(here("1_raw_data", paste0(amptk_prefix, ".cluster.taxonomy.txt")))
-rownames(otu_tax) <- otu_tax$X.OTUID
+otu_tax_11 <- read.delim(here("1_raw_data", paste0(amptk_prefix, "_2020_11.cluster.taxonomy.txt")))
+otu_tax_11$X.OTUID <- paste0(otu_tax_11$X.OTUID, "_11") # delineate as Nov seq run
+rownames(otu_tax_11) <- otu_tax_11$X.OTUID
 
 # The taxonomy result from AMPtk is in one long string of text. This is splitting up the string
 # and filling in a bunch of different columns. 
-for(i in 1:nrow(otu_tax)){
-  temp <- otu_tax$taxonomy[i]
+for(i in 1:nrow(otu_tax_11)){
+  temp <- otu_tax_11$taxonomy[i]
   temp2 <- strsplit(temp, "\\|")[[1]][3]
   temp3 <- strsplit(temp2, ":")[[1]][2]
-  otu_tax$search_hit[i] <- strsplit(temp, "\\|")[[1]][1]
-  otu_tax$hit_score[i] <- strsplit(temp, "\\|")[[1]][2]
-  otu_tax$database[i] <- strsplit(temp2, ":")[[1]][1]
-  otu_tax$accession[i] <- strsplit(temp3, ";")[[1]][1]
-  otu_tax$kingdom[i] <- strsplit(strsplit(temp2, "k:")[[1]][2], ",")[[1]][1]
-  otu_tax$phylum[i] <- strsplit(strsplit(temp2, "p:")[[1]][2], ",")[[1]][1]
-  otu_tax$class[i] <- strsplit(strsplit(temp2, "c:")[[1]][2], ",")[[1]][1]
-  otu_tax$order[i] <- strsplit(strsplit(temp2, "o:")[[1]][2], ",")[[1]][1]
-  otu_tax$family[i] <- strsplit(strsplit(temp2, "f:")[[1]][2], ",")[[1]][1]
-  otu_tax$genus[i] <- strsplit(strsplit(temp2, "g:")[[1]][2], ",")[[1]][1]
-  otu_tax$species[i] <- strsplit(strsplit(temp2, "s:")[[1]][2], ",")[[1]][1]		
+  otu_tax_11$search_hit[i] <- strsplit(temp, "\\|")[[1]][1]
+  otu_tax_11$hit_score[i] <- strsplit(temp, "\\|")[[1]][2]
+  otu_tax_11$database[i] <- strsplit(temp2, ":")[[1]][1]
+  otu_tax_11$accession[i] <- strsplit(temp3, ";")[[1]][1]
+  otu_tax_11$kingdom[i] <- strsplit(strsplit(temp2, "k:")[[1]][2], ",")[[1]][1]
+  otu_tax_11$phylum[i] <- strsplit(strsplit(temp2, "p:")[[1]][2], ",")[[1]][1]
+  otu_tax_11$class[i] <- strsplit(strsplit(temp2, "c:")[[1]][2], ",")[[1]][1]
+  otu_tax_11$order[i] <- strsplit(strsplit(temp2, "o:")[[1]][2], ",")[[1]][1]
+  otu_tax_11$family[i] <- strsplit(strsplit(temp2, "f:")[[1]][2], ",")[[1]][1]
+  otu_tax_11$genus[i] <- strsplit(strsplit(temp2, "g:")[[1]][2], ",")[[1]][1]
+  otu_tax_11$species[i] <- strsplit(strsplit(temp2, "s:")[[1]][2], ",")[[1]][1]		
 }
 
 # Replace database mismatches caused by matches that aren't from BOLD records
-otu_tax$database <- gsub("None;k", "None", otu_tax$database)
-otu_tax$accession <- gsub("Animalia,p", "None", otu_tax$accession)
+otu_tax_11$database <- gsub("None;k", "None", otu_tax_11$database)
+otu_tax_11$accession <- gsub("Animalia,p", "None", otu_tax_11$accession)
 
 # For phyloseq this has to be added as a matrix rather than data frame    
-otu_tax <- as.matrix(otu_tax)
+otu_tax_11 <- as.matrix(otu_tax_11)
 # This is saving just the taxonomic ranks rather than the database info.
-otu_tax2 <- otu_tax[, 10:16]
+otu_tax_11_2 <- otu_tax_11[, 10:16]
 
 # Add in the sample information to each sample
 s_info <- read.delim(here("1_raw_data", "tres_sample_info.txt"))    # prepared outside of AMPtk
-map2 <- join(map, s_info, "sampleID", "left", "first")
-rownames(map2) <- map2$X.SampleID
+map_11_2 <- join(map_11, s_info, "sampleID", "left", "first")
+rownames(map_11_2) <- map_11_2$X.SampleID
 
 # Print out some summary information about this dataset
-summary_table <- map2 %>% dplyr::count(site, age.1, cap_num)
+summary_table <- map_11_2 %>% dplyr::count(site, age.1, cap_num)
 
 # This will identify how many samples there are in each category of site,
 # age, and capture number.
 
 # This will also identify samples that don't match the metadata file and write them as a separate output
 # missing <- subset(map2, is.na(map2$band) == TRUE)
-# write.table(missing, "missing_info.txt", sep = "\t")      
+# write.table(missing, "missing_info.txt", sep = "\t") 
+
+############################# December Run (12)
+
+# Load the number of reads by taxa per sample table. Format for phyloseq.
+otu_ab_12 <- read.delim(here("1_raw_data", paste0(amptk_prefix, "_2020_12.cluster.otu_table.txt")))
+otu_ab_12$X.OTU.ID <- paste0(otu_ab_12$X.OTU.ID, "_12") # delineate as Dec seq run
+rownames(otu_ab_12) <- otu_ab_12$X.OTU.ID   # give rownames from sample names
+otu_ab_12 <- otu_ab_12[, 2:ncol(otu_ab_12)]    # remove the column of sample names
+
+# Read the mapping table
+# this 'mapping' table from AMPtk is mostly blank but has all the sample
+# names so it can be joined to actual sample metadata. It's also possible
+# to merge in the sample metadata within the AMPtk pipeline.
+map_12 <- read.delim(here("1_raw_data", paste0(amptk_prefix, "_2020_12.mapping_file.txt")))
+
+for(i in 1:nrow(map_12)){
+  map_12$sampleID[i] <- strsplit(map_12$X.SampleID[i], "x")[[1]][2]
+}
+
+# write.table(map_12, "map_12.txt", sep = "\t") # if you want to save a copy of the mapping file
+
+# Read the otu taxonomy table
+otu_tax_12 <- read.delim(here("1_raw_data", paste0(amptk_prefix, "_2020_12.cluster.taxonomy.txt")))
+otu_tax_12$X.OTUID <- paste0(otu_tax_12$X.OTUID, "_12") # delineate as Dec seq run
+rownames(otu_tax_12) <- otu_tax_12$X.OTUID
+
+# The taxonomy result from AMPtk is in one long string of text. This is splitting up the string
+# and filling in a bunch of different columns. 
+for(i in 1:nrow(otu_tax_12)){
+  temp <- otu_tax_12$taxonomy[i]
+  temp2 <- strsplit(temp, "\\|")[[1]][3]
+  temp3 <- strsplit(temp2, ":")[[1]][2]
+  otu_tax_12$search_hit[i] <- strsplit(temp, "\\|")[[1]][1]
+  otu_tax_12$hit_score[i] <- strsplit(temp, "\\|")[[1]][2]
+  otu_tax_12$database[i] <- strsplit(temp2, ":")[[1]][1]
+  otu_tax_12$accession[i] <- strsplit(temp3, ";")[[1]][1]
+  otu_tax_12$kingdom[i] <- strsplit(strsplit(temp2, "k:")[[1]][2], ",")[[1]][1]
+  otu_tax_12$phylum[i] <- strsplit(strsplit(temp2, "p:")[[1]][2], ",")[[1]][1]
+  otu_tax_12$class[i] <- strsplit(strsplit(temp2, "c:")[[1]][2], ",")[[1]][1]
+  otu_tax_12$order[i] <- strsplit(strsplit(temp2, "o:")[[1]][2], ",")[[1]][1]
+  otu_tax_12$family[i] <- strsplit(strsplit(temp2, "f:")[[1]][2], ",")[[1]][1]
+  otu_tax_12$genus[i] <- strsplit(strsplit(temp2, "g:")[[1]][2], ",")[[1]][1]
+  otu_tax_12$species[i] <- strsplit(strsplit(temp2, "s:")[[1]][2], ",")[[1]][1]		
+}
+
+# Replace database mismatches caused by matches that aren't from BOLD records
+otu_tax_12$database <- gsub("None;k", "None", otu_tax_12$database)
+otu_tax_12$accession <- gsub("Animalia,p", "None", otu_tax_12$accession)
+
+# For phyloseq this has to be added as a matrix rather than data frame    
+otu_tax_12 <- as.matrix(otu_tax_12)
+# This is saving just the taxonomic ranks rather than the database info.
+otu_tax_12_2 <- otu_tax_12[, 10:16]
+
+# Add in the sample information to each sample
+s_info <- read.delim(here("1_raw_data", "tres_sample_info.txt"))    # prepared outside of AMPtk
+map_12_2 <- join(map_12, s_info, "sampleID", "left", "first")
+rownames(map_12_2) <- map_12_2$X.SampleID
+
+# Print out some summary information about this dataset
+summary_table <- map_12_2 %>% dplyr::count(site, age.1, cap_num)
+
+# This will identify how many samples there are in each category of site,
+# age, and capture number.
+
+# This will also identify samples that don't match the metadata file and write them as a separate output
+# missing <- subset(map_12_2, is.na(map_12_2$band) == TRUE)
+# write.table(missing, "missing_info.txt", sep = "\t") 
 
 ################################################################################
 # Build initial phyloseq object ----
 
-OTU = otu_table(otu_ab, taxa_are_rows = TRUE)
+## Combine tax tables from both sequencing runs
+otu_tax2 <- rbind(otu_tax_11_2, otu_tax_12_2)
 TAX = tax_table(otu_tax2)
+
+## Combine map files from both sequencing runs
+map2 <- rbind(map_11_2, map_12_2)
 SAM = sample_data(map2)
+
+## Combine otu tables from both sequencing runs
+# To do this, we'll have to create new columns that match
+nov_names <- colnames(otu_ab_11)
+dec_names <- colnames(otu_ab_12)
+otu_ab_11[dec_names] <- 0 # fill new columns with 0
+otu_ab_12[nov_names] <- 0 # fill new columns with 0
+otu_ab <- rbind(otu_ab_11, otu_ab_12)
+otu_ab <- as.matrix(otu_ab)
+OTU = otu_table(otu_ab, taxa_are_rows = TRUE)
 
 coi_ps <- phyloseq(OTU, TAX, SAM)
 
-# Check sample effort ----
-
 # Subset just to arthropods
 coi_ps2 <- subset_taxa(coi_ps, phylum == "Arthropoda")
+
+# Extract the unique arthropod families found in the dataset
+unique_families <- get_taxa_unique(coi_ps2, taxonomic.rank = "family")
+# Save file with list of unique families to use to research aquatic vs.
+# terrestrial families.
+write.csv(unique_families, here("5_other_outputs/unique_families.csv"))
+# This file was then taken out of R to research aquatic vs. terrestrial
+# families, and will be re-imported later.
+
+# Subset just to the data we want for this project
+coi_ps2 <- subset_samples(coi_ps2, parental_investment_proj == "yes")
+
+# Extract the unique arthropod families found in the dataset JUST for parental
+# investment project
+unique_families_pi <- get_taxa_unique(coi_ps2, taxonomic.rank = "family")
+# Save file with list of unique families to use to research aquatic vs.
+# terrestrial families.
+write.csv(unique_families_pi, here("5_other_outputs/unique_families_pi.csv"))
+# This file was then taken out of R to research aquatic vs. terrestrial
+# families, and will be re-imported later.
+
+################################################################################
+# Check sample effort ----
 
 # Histogram of sample reads. This is counting a sum of arthropod reads for each sample.
 depth <- data.frame(as(sample_data(coi_ps2), "data.frame"),
@@ -121,13 +231,23 @@ p2 <- p + facet_grid(~ age)     # same but splitting out adult/nestling/negative
 ggsave(here("3_r_scripts/total_reads.png"), plot = p, width = 8, height = 4.5, device = "png")
 ggsave(here("3_r_scripts/total_reads_split.png"), plot = p2, width = 8.2, height = 4, device = "png")
 
-# Extract the unique arthropod families found in the dataset
-unique_families <- get_taxa_unique(coi_ps2, taxonomic.rank = "family")
-# Save file with list of unique families to use to research aquatic vs.
-# terrestrial families.
-write.csv(unique_families, here("5_other_outputs/unique_families.csv"))
-# This file was then taken out of R to research aquatic vs. terrestrial
-# families, and will be re-imported later.
+# Histogram of sample reads across captive nestling days
+coi_ps2_captive <- subset_samples(coi_ps2, site == "Gut_passage") # extract just gut passage time birds
+depth <- data.frame(as(sample_data(coi_ps2_captive), "data.frame"),
+                    TotalReads = sample_sums(coi_ps2_captive), keep.rownames = TRUE)
+p_cap <- ggplot(depth, aes(log(TotalReads))) + geom_histogram(fill = "slateblue") + 
+  ylab("Count of Samples") + xlab("log(Reads)") +
+  theme_classic() + geom_vline(xintercept = log(150), linetype = "dashed", col = "coral3", size = 1) +
+  xlim(0,10)
+p2_cap <- p_cap + facet_grid(~ age.1) # same but splitting out adult/nestling/negative_control
+# set order for ages for neatness in plots 
+age_order = c("6", "7", "8", "9", "10", "11", "12")
+p2_cap$data$age.1 <- as.character(p2_cap$data$age.1)
+p2_cap$data$age.1 <- factor(p2_cap$data$age.1, levels=age_order)
+
+# Save the histograms to file to be added to the markdown
+ggsave(here("3_r_scripts/total_reads_cap.png"), plot = p_cap, width = 8, height = 4.5, device = "png")
+ggsave(here("3_r_scripts/total_reads_split_cap.png"), plot = p2_cap, width = 8.2, height = 4, device = "png")
 
 # Rarefy to even depth of 150
 # running this would rarefy to an even depth moving forward
@@ -169,12 +289,6 @@ coi_ra <- transform_sample_counts(coi_ps2, function(x) x / sum(x))
 
 ## Filter out taxa with relative abundance values below some threshold
 coi_ra2 <- filter_taxa(coi_ra, function(x) mean(x) > 1e-5, TRUE)
-
-# Take out the old test samples
-coi_ra2 <- subset_samples(coi_ra2, age.1 != "old")
-
-# Take out the testing samples from 2019 (were labelled as M19NXXX and do not have nest information)
-coi_ra2 <- subset_samples(coi_ra2, is.na(nest) == FALSE)
 
 # Transform to presence absence
 coi_pa <- transform_sample_counts(coi_ra2, function(x) ceiling(x))
